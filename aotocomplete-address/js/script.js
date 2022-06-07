a8a905d1639d9711c04b8c13f3137a1b4caf2fb5
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-07 07:29:39 -0300

    Autocomplete: Validate CEP input

--- aotocomplete-address/js/script.js
+++ aotocomplete-address/js/script.js
@@ -4,4 +4,21 @@ const addressInput = document.querySelector("#address");
 const cityInput = document.querySelector("#city");
 const neighborhoodInput = document.querySelector("#neighborhood");
 const regionInput = document.querySelector("#region");
-const formInputs = document.querySelector("[data-input]");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+//Validate CEP input
+cepInput.addEventListener("keypress", (e) => {
+    const onlyNumbers = /[0-9]|\./;
+    const key = String.fromCharCode(e.keyCode);
+
+    console.log(key);
+    console.log(onlyNumbers.test(key));
+
+    // allow only numbers
+    if (!onlyNumbers.test(key)) {
+        e.preventDefault();
+        return;
+    }
+});
