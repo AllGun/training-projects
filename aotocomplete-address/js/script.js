cefb9746706886adb620dbe12f989df84b90e874
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-07 19:33:53 -0300

    Autocomplete: Get address event

--- aotocomplete-address/js/script.js
+++ aotocomplete-address/js/script.js
@@ -10,15 +10,27 @@ const closeButton = document.querySelector("#close-message");
 
 //Validate CEP input
 cepInput.addEventListener("keypress", (e) => {
-    const onlyNumbers = /[0-9]/;
-    const key = String.fromCharCode(e.keyCode);
+  const onlyNumbers = /[0-9]/;
+  const key = String.fromCharCode(e.keyCode);
 
-    // allow only numbers
-    if (!onlyNumbers.test(key)) {
-        e.preventDefault();
-        return;
-    }
+  // allow only numbers
+  if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+  }
 });
 
 // Get address event
-cepInput.addEventListener("keyup", )
+cepInput.addEventListener("keyup", (e) => {
+  const inputValue = e.target.value;
+
+  //Chek if we have the correct length
+  if (inputValue.length === 8) {
+    getAddress(inputValue);
+  }
+});
+
+// Get customer address from API
+const getAddress = async (cep) => {
+    console.log(cep);
+};
