68dfd86f3dc22fd997644016ca6e1cab83ee6f27
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-20 18:41:43 -0300

    class Calc done

--- calc/app/script.js
+++ calc/app/script.js
@@ -2,3 +2,16 @@ const previousOperationText = document.querySelector("#previous-operation");
 const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+    }
+
+    // add digit to calculator screen
+    addDigit(digit) {
+        console.log(digit);
+    }
+}
+
