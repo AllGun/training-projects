2af993c1714fbe0e0ecfd5fdb3fa450de6ea66f9
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-22 11:11:49 -0300

    btn Pause

--- stopwatch/app/script.js
+++ stopwatch/app/script.js
@@ -2,3 +2,26 @@ const span = document.querySelector("span");
 const btnStart = document.querySelector("#btnStart");
 const btnPause = document.querySelector("#btnPause");
 const btnClean = document.querySelector("#btnClean");
+
+var interval;
+
+btnStart.onclick = () => {
+    btnStart.disabled = true;
+    btnPause.disabled = false;
+
+    var value = parseFloat(span.innerHTML);
+
+    interval = setInterval(() => {
+        value += 0.1;
+
+        span.innerHTML = value.toFixed(1);
+    }, 100);
+}
+
+btnPause.onclick = () => {
+    btnStart.disabled = false;
+    btnPause.disabled = true;
+
+    clearInterval(interval);
+}
+
