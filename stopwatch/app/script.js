27851bae358f0a901a679ca544567760d1e227a8
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-06-22 10:05:37 -0300

    const js

---
+++ stopwatch/app/script.js
@@ -0,0 +1,4 @@
+const span = document.querySelector("span");
+const btnStart = document.querySelector("#btnStart");
+const btnPause = document.querySelector("#btnPause");
+const btnClean = document.querySelector("#btnClean");
